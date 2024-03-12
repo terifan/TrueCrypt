55db2be8a9f664baad6646e74177b3466e202b38
terifan <pinjata69@gmail.com>
2024-03-12 16:38:08 +0100

    update

--- src/example/Example.java
+++ src/example/Example.java
@@ -1,9 +1,11 @@
 package example;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.function.Consumer;
 import org.terifan.fat32.FatFile;
+import org.terifan.fat32.FatFileSystem;
 import org.terifan.pagestore.FilePageStore;
 import org.terifan.truecrypt.TrueCryptPageStore;
 
@@ -19,19 +21,19 @@ public class Example
 				System.out.println(t);
 			};
 
-			TrueCryptPageStore.create(new FilePageStore(new File("d:/test.tc"), false, 512), 1000, "password", TrueCryptPageStore.CipherOption.AES_TWOFISH_SERPENT, TrueCryptPageStore.DigestOption.SHA512, prog);
+//			TrueCryptPageStore.create(new FilePageStore(new File("d:/test.tc"), false, 512), 1000, "password", TrueCryptPageStore.CipherOption.AES_TWOFISH_SERPENT, TrueCryptPageStore.DigestOption.SHA512, prog);
 
-//			try (FatFileSystem fs = new FatFileSystem(TrueCryptPageStore.open(new FilePageStore(new File("d:/test.tc")), "password")))
-//			{
-//				FatFile file = fs.getFile("/Wallpapers Fantasy/darth-vader-hd-1080p-wallpaper-full-hd-wallpaper.jpg");
-//
-//				try (FileOutputStream fos = new FileOutputStream("d:/test.jpg"))
-//				{
-//					fos.write(file.readAll());
-//				}
-//
-//				iterate(fs.getFile(""));
-//			}
+			try (FatFileSystem fs = new FatFileSystem(TrueCryptPageStore.open(new FilePageStore(new File("d:/test.tc")), "password")))
+			{
+				FatFile file = fs.getFile("/Pictures/2zw4g6fjnx231.jpg");
+
+				try (FileOutputStream fos = new FileOutputStream("d:/test.jpg"))
+				{
+					fos.write(file.readAll());
+				}
+
+				iterate(fs.getFile(""));
+			}
 		}
 		catch (Exception e)
 		{
